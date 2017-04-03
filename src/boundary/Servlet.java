bd64825a9c742bbdc060d88c21bbe84d432c9857
Erika Luz <LAL57795@uga.edu>
2017-04-02 23:01:03 -0400

    Added Servlet

---
+++ src/boundary/Servlet.java
@@ -0,0 +1,54 @@
+package boundary;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// import boundary.TemplateProcessor;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapperBuilder;
+import freemarker.template.SimpleHash;
+
+/**
+ * Servlet implementation class Imdb2Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet 
+{
+	//VARIABLES
+		private static final long serialVersionUID = 1L;	
+		private String templateDir = "/WEB-INF/templates";
+//		private TemplateProcessor processor;
+		
+	    //@see HttpServlet#HttpServlet() 
+			public Servlet() 
+			{
+				super();
+			} //end of constructor
+	    
+		//@see Servlet#init(ServletConfig) 
+			public void init(ServletConfig config) throws ServletException 
+			{
+				super.init(config);
+//				processor = new TemplateProcessor(templateDir, getServletContext());
+			} //end of init
+			
+		//@see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) 
+			protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+			{
+					
+			} // end of doGet
+	
+		
+		//@see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+			protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+			{
+				doGet(request, response);
+			} //end of doPost
+}
