869b5e46c8a131d633edac0ac4f4c1b6d27cd0ef
Austin Salyers <a.salyers231@gmail.com>
2017-04-09 21:16:24 -0400

    cabin manager restore method

--- src/persistence/CabinManager.java
+++ src/persistence/CabinManager.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
 
 import object.Cabin;
 
@@ -86,4 +90,134 @@ public class CabinManager {
 	    	e.printStackTrace();
 		}
 	}
+	
+	public static List<Cabin> restore( Cabin modelCabin )
+	{
+		String  selectCabinSql = "select id, address, city, state, description, bedroom_count, bath_count, max_occupancy from cabin"; 
+		Statement    statement = null;
+		StringBuffer query = new StringBuffer( 100 );
+		StringBuffer condition = new StringBuffer( 100 );
+		List<Cabin> cabins = new LinkedList<Cabin>();
+		Connection conn = DbAccessImpl.connect();
+
+
+		condition.setLength( 0 );
+
+		// form the query based on the given Election object instance
+		query.append( selectCabinSql );
+		//voteCount, office, isPartisan
+		if( modelCabin != null ) {
+			if( modelCabin.getId() >= 0 ) // id is unique, so it is sufficient to get a ballot
+				query.append( " where id = " + modelCabin.getId() );
+			else {				
+				if( modelCabin.getAddress() != null )
+					condition.append( " address = '" + modelCabin.getAddress() + "'");
+				if( modelCabin.getCity() != null ) {
+					if ( condition.length() > 0 )
+						condition.append( " and");
+					condition.append(" city = '" + modelCabin.getCity() + "'");
+				}
+				if( modelCabin.getState() != null ) {
+					if ( condition.length() > 0 )
+						condition.append( " and");
+					condition.append(" state = '" + modelCabin.getState() + "'");
+				}
+				if( modelCabin.getDescription() != null ) {
+					if ( condition.length() > 0 )
+						condition.append( " and");
+					condition.append(" description = '" + modelCabin.getDescription() + "'");
+				}
+				if( modelCabin.getBedroomCount() >= 0 ) {
+					if ( condition.length() > 0 )
+						condition.append( " and");
+					condition.append(" bedroom_count = " + modelCabin.getBedroomCount() );
+				}
+				if( modelCabin.getBathCount() >= 0 ) {
+					if ( condition.length() > 0 )
+						condition.append( " and");
+					condition.append(" bath_count = " + modelCabin.getBathCount() );
+				}
+				if( modelCabin.getMaxOccupancy() >= 0 ) {
+					if ( condition.length() > 0 )
+						condition.append( " and");
+					condition.append(" max_Occupancy = " + modelCabin.getMaxOccupancy() );
+				}
+				if( condition.length() > 0 ) {
+					query.append(  " where " );
+					query.append( condition );
+				}
+			}
+		}
+		
+		try {			
+			statement = conn.createStatement();
+			
+			// retrieve the persistent Election objects
+			if( statement.execute( query.toString() ) ) { // statement returned a result
+				
+				int   	id;
+				String 	address;
+				String 	city;
+				String 	state;
+				String 	description;
+				int		bedroomCount;
+				float	bathCount;
+				int		maxOccupancy;
+				
+				
+				ResultSet rs = statement.getResultSet();
+
+				while( rs.next() ) {
+
+					id = rs.getInt(1);
+					address = rs.getString(2);
+					city = rs.getString(3);
+					state = rs.getString(4);
+					description = rs.getString(5);
+					bedroomCount = rs.getInt(6);
+					bathCount = rs.getFloat(7);
+					maxOccupancy = rs.getInt(8);
+
+					Cabin cabin = new Cabin();
+					cabin.setId( id );
+					cabin.setAddress( address );
+					cabin.setCity( city );
+					cabin.setState( state );
+					cabin.setDescription( description );
+					cabin.setBedroomCount( bedroomCount );
+					cabin.setBathCount( bathCount );
+					cabin.setMaxOccupancy( maxOccupancy );
+					cabin.setUser( null );	
+					cabin.setAmenities( null );
+					
+					cabins.add( cabin );
+				}
+				return cabins;
+			}
+		}
+		catch( SQLException e ) {      
+			//throw new CompareCabinsException( "ClubManager.restore: Could not restore persistent Club objects; Root cause: " + e );
+			e.printStackTrace();
+		}
+
+		// if this point is reached, it is an error
+		//throw new CompareCabinsException( "ElectionManager.restore: Could not restore persistent Election objects" );
+		
+		// temporary, correctly handled by throwing exception at end of file like above
+		cabins = null; 
+		return cabins;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
