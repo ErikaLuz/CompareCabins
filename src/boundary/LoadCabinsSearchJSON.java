661ebde65178884207db508727b911d74925265a
Austin Salyers <a.salyers231@gmail.com>
2017-04-26 23:43:48 -0400

    cabin search load

--- src/boundary/LoadCabinsSearchJSON.java
+++ src/boundary/LoadCabinsSearchJSON.java
@@ -16,25 +16,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import exception.CCException;
+import logic.LogicLayerImplShep;
+import object.Amenities;
+import object.Availability;
 import object.Cabin;
 import object.CabinPicture;
 import persistence.CabinManager;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-
 /**
- * Servlet implementation class LoadCabinsJSON
+ * Servlet implementation class LoadCabinsSearchJSON
  */
-@WebServlet("/LoadCabinsJSON")
-public class LoadCabinsJSON extends HttpServlet {
+@WebServlet("/LoadCabinsSearchJSON")
+public class LoadCabinsSearchJSON extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoadCabinsJSON() {
+    public LoadCabinsSearchJSON() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -43,18 +42,52 @@ public class LoadCabinsJSON extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		String[] primaryPhotoFilePath = null;
         
         // set up the response writer
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter toClient = response.getWriter();
         
-        //cabinId = Integer.parseInt( request.getParameter("cabinId") );
+        String hasLake = request.getParameter("hasLake");
+		String hasRiver = request.getParameter("hasRiver");
+		String hasPool = request.getParameter("hasPool");
+		String hasHotTub = request.getParameter("hasHotTub");
+		String hasWifi = request.getParameter("hasWifi");
+		String hasAirConditioning = request.getParameter("hasAirConditioning");
+		String hasWasherDryer = request.getParameter("hasWasherDryer");
+		String allowsPets = request.getParameter("allowsPets");
+		String allowsSmoking = request.getParameter("allowsSmoking");
+		String startDateString = request.getParameter("startAvailability");
+		String endDateString = request.getParameter("endAvailability");
+		
+		Availability start = parseDateString( startDateString );
+		Availability end = parseDateString( endDateString );
+		
+		Amenities amenities = new Amenities();
+		if(hasLake != null)
+			amenities.setHasLake(true);
+		if(hasRiver != null)
+			amenities.setHasRiver(true);
+		if(hasPool != null)
+			amenities.setHasLake(true);
+		if(hasHotTub != null)
+			amenities.setHasHotTub(true);
+		if(hasWifi != null)
+			amenities.setHasWifi(true);
+		if(hasAirConditioning != null)
+			amenities.setHasAirConditioning(true);
+		if(hasWasherDryer != null)
+			amenities.setHasWasherDryer(true);
+		if(allowsPets != null)
+			amenities.setAllowsPets(true);
+		if(allowsSmoking != null)
+			amenities.setAllowsSmoking(true);
+        
+		
         
         List<Cabin> cabins = null;
 		try {
-			cabins = CabinManager.restore(null);
+			cabins = LogicLayerImplShep.search( amenities, start, end );
 	
 	        primaryPhotoFilePath = new String[ cabins.size() ];
 	        
@@ -101,5 +134,23 @@ public class LoadCabinsJSON extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	private Availability parseDateString( String dateString )
+	{
+		String year = dateString.substring(0,4);
+		String month = dateString.substring(5,7);
+		String day = dateString.substring(8,10);
+		
+		int yearInt = Integer.parseInt(year);
+		int monthInt = Integer.parseInt(month);
+		int dayInt = Integer.parseInt(day);
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(yearInt, monthInt, dayInt);
+		Availability availability = new Availability();
+		availability.setDate( cal );
+		
+		return availability;
+	}
+
 
 }
